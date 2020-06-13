740a0b2877ddb23e6a93500a2a9c8427dcdcd57e
nirwanjaya <66889971+nirwanjaya@users.noreply.github.com>
2020-06-14 06:47:59 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,13 @@ self.addEventListener('install', function(event) {
         cache.addAll([
           '/',
           '/index.html',
+          '/welcomescreen.html',
           '/src/css/app.css',
-          '/src/js/app.js'
+          '/src/css/bootstrap.css',
+          '/src/js/app.js',
+          '/src/js/jquery.min.js',
+          '/src/js/popper.min.js',
+          '/src/js/bootstrap.min.js'
         ])
       })
   );
