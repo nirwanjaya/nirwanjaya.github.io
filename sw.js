32a5fb2f0abad3521822b4c6495e8b788a8666a7
nirwanjaya <66889971+nirwanjaya@users.noreply.github.com>
2020-06-14 08:43:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,7 +15,14 @@ self.addEventListener('install', function(event) {
           '/src/js/popper.min.js',
           '/src/js/bootstrap.min.js',
           '/src/images/homescreen.png',
+          '/src/images/icons/icon-72x72.png',
+          '/src/images/icons/icon-96x96.png',
+          '/src/images/icons/icon-128x128.png',
           '/src/images/icons/icon-144x144.png',
+          '/src/images/icons/icon-152x152.png',
+          '/src/images/icons/icon-192x192.png',
+          '/src/images/icons/icon-384x384.png',
+          '/src/images/icons/icon-512x512.png',
           '/src/images/char1.gif',
           '/src/images/char2.gif'
         ])
