37a24bdd5ef2d8326217722e32bbeaf21ab62422
nirwanjaya <66889971+nirwanjaya@users.noreply.github.com>
2020-06-14 08:21:01 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,15 @@ var button = document.querySelector('button');
 
 navigator.serviceWorker.register('/sw.js');
 
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+
 function animate() {
   title.classList.remove('animate-in');
   for (var i = 0; i < courseFeatureElements.length; i++) {
